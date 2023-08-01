25f373531fbcee9e70664c601df5d71dd6a4a280
Andrei Varcus <andrei.varcus@yahoo.com>
2023-08-02 00:21:29 +0300

    finished navbar on project task manager

--- src/components/Dashboard/Project.jsx
+++ src/components/Dashboard/Project.jsx
@@ -1,11 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import projectBG from '../../media/projectBG.png';
+import styles from './Project.module.css';
 
 const Project = () => {
     const { id } = useParams();
     return (
         <section>
-            <div>Project: {id}</div>
+            <nav className={styles.project}>
+                <img src={projectBG} alt={id} />
+                <span className={styles.projectName}>{id}</span>
+                <div className={styles.projectGroupMenu}>
+                    <div className={styles.projectMenu}>
+                        <FontAwesomeIcon icon="fa-solid fa-border-all" />
+                        Board
+                    </div>
+                    <div className={styles.projectMenu}>
+                        <FontAwesomeIcon icon="fa-solid fa-chart-column" />
+                        Charts
+                    </div>
+                    <div className={styles.projectMenu}>
+                        <FontAwesomeIcon icon="fa-solid fa-users" />
+                        Members&#128317;
+                    </div>
+                </div>
+            </nav>
+            <div className={styles.sectionBG}>hello</div>
         </section>
     );
 };
