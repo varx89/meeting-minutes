c8a14d25c51e7c7d16b030a1a3b5dc61fb9179b1
Andrei Varcus <andrei.varcus@yahoo.com>
2023-08-02 23:27:55 +0300

    finished task management article design

--- src/components/Dashboard/Project.jsx
+++ src/components/Dashboard/Project.jsx
@@ -1,7 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import projectBG from '../../media/projectBG.png';
+import {
+    default as projectBG,
+    default as taskImage,
+} from '../../media/projectBG.png';
 import styles from './Project.module.css';
 
 const Project = () => {
@@ -26,7 +29,107 @@ const Project = () => {
                     </div>
                 </div>
             </nav>
-            <div className={styles.sectionBG}>hello</div>
+            <div className={styles.sectionBG}>
+                <div>
+                    <div className={styles.taskmng}>
+                        <span>To do</span>
+                        <div className={styles.taskAdd}>
+                            <FontAwesomeIcon
+                                icon="fa-solid fa-circle-plus"
+                                style={{ color: 'gray' }}
+                            />
+                        </div>
+                        <article className={styles.taskRow}>
+                            <div className={styles.taskTitle}>
+                                <span>Media Outlets</span>
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-ellipsis"
+                                    style={{ color: 'gray' }}
+                                />
+                            </div>
+
+                            <div className={styles.taskClose}>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon="fa-regular fa-calendar"
+                                        style={{ paddingRight: '0.2rem' }}
+                                    />
+                                    10.04.23
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon icon="fa-solid fa-flag" />
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-hotjar"
+                                        style={{
+                                            color: '#ff0000',
+                                            paddingLeft: '0.25rem',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </article>
+                        <article className={styles.taskRow}>
+                            <img src={taskImage} alt="" />
+                            <div className={styles.taskTitle}>
+                                <span>Media Outlets</span>
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-ellipsis"
+                                    style={{ color: 'gray' }}
+                                />
+                            </div>
+
+                            <div className={styles.taskClose}>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon="fa-regular fa-calendar"
+                                        style={{ paddingRight: '0.2rem' }}
+                                    />
+                                    10.04.23
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon icon="fa-solid fa-flag" />
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-hotjar"
+                                        style={{
+                                            color: '#ff0000',
+                                            paddingLeft: '0.25rem',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </article>
+                        <article className={styles.taskRow}>
+                            <div className={styles.taskTitle}>
+                                <span>Media Outlets</span>
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-ellipsis"
+                                    style={{ color: 'gray' }}
+                                />
+                            </div>
+
+                            <div className={styles.taskClose}>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon="fa-regular fa-calendar"
+                                        style={{ paddingRight: '0.2rem' }}
+                                    />
+                                    10.04.23
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon icon="fa-solid fa-flag" />
+                                    <FontAwesomeIcon
+                                        icon="fa-brands fa-hotjar"
+                                        style={{
+                                            color: '#ff0000',
+                                            paddingLeft: '0.25rem',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
